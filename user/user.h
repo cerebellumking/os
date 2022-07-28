d788de5fb574e05e8d95cf7dbe42d95676242bf7
cerebellumiking <1805219550@qq.com>
2022-07-28 08:33:59 +0800

    完成makefile与grade

--- user/user.h
+++ user/user.h
@@ -1,6 +1,7 @@
 struct stat;
 struct rtcdate;
 
+typedef unsigned int uint;
 // system calls
 int fork(void);
 int exit(int) __attribute__((noreturn));
@@ -40,3 +41,4 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
