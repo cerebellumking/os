008efd8ca2b7738e8949dd55ea961a9bc1e9961c
cerebellumiking <1805219550@qq.com>
2022-07-29 08:17:09 +0800

    print page tables

--- kernel/defs.h
+++ kernel/defs.h
@@ -162,6 +162,7 @@ pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
 uint64          uvmalloc(pagetable_t, uint64, uint64);
 uint64          uvmdealloc(pagetable_t, uint64, uint64);
+void            vmprint(pagetable_t);
 int             uvmcopy(pagetable_t, pagetable_t, uint64);
 void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
