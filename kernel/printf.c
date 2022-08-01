39783710b04d044d726b91a48962b3038a6b691e
cerebellumiking <1805219550@qq.com>
2022-08-02 07:55:19 +0800

    lab4

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,20 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 fp, ra, top, bottom;
+  printf("backtrace:\n");
+  fp = r_fp();
+  top = PGROUNDUP(fp);
+  bottom = PGROUNDDOWN(fp);
+  while (fp < top && fp > bottom)
+  {
+    ra = *(uint64 *)(fp-8);
+    printf("%p\n", ra);
+    fp = *(uint64 *)(fp-16);
+  }
 }
--- kernel/proc.c
+++ kernel/proc.c
@@ -134,7 +134,8 @@ found:
     release(&p->lock);
     return 0;
   }
-
+  p->ticks = 0;
+  p->in_handler = 0;
   // Set up new context to start executing at forkret,
   // which returns to user space.
   memset(&p->context, 0, sizeof(p->context));
