2edcac541354f6ed853e39ff774ae20e245e2f40
cerebellumiking <1805219550@qq.com>
2022-08-03 08:24:54 +0800

    lab5

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,7 +63,9 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-
+void            refcinc(void *);
+void            refcdec(void *);
+int             getrefc(void *);
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -145,7 +147,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
-
+int             cow_alloc(pagetable_t, uint64);
 // uart.c
 void            uartinit(void);
 void            uartintr(void);
@@ -170,7 +172,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-
+pte_t*          walk(pagetable_t, uint64, int);
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
